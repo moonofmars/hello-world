30be48527bf89dcce1b7821ea7abd4f0b234889b
zhuernm <zhuernm@hotmail.com>
2016-09-12 11:51:14 +0800

    fetch url

---
+++ fetch/fetch.go
@@ -0,0 +1,22 @@
+// fetch
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close() //avoid leaking resources
+	}
+}
